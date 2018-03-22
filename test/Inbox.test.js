3549d38bdeb612b9c7d1399c724bce6d79f4f138
zeffon <zephoony@gmail.com>
2018-03-22 17:04:37 +0800

    commit gitignore and test file

---
+++ test/Inbox.test.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+
+let accounts;
+let inbox;
+const INITIAL_MESSAGE = 'Hi there!';
+
+beforeEach(async () => {
+
+});
+
+describe('', () => {
+  it('', () => {
+
+  });
+});
